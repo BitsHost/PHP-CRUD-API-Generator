2288f9c8e6e2fea1077b3f0c7bcc2c704357a442
BitsHost <dadryan26@yahoo.com>
2025-11-12 21:40:52 +0200

    up

--- rector.php
+++
@@ -1,9 +0,0 @@
-<?php
-declare(strict_types=1);
-
-// Deprecated: Migration tooling removed in v2.0.0-dev
-// This file is intentionally inert. If executed, it will explain why.
-
-throw new RuntimeException(
-    'rector.php has been removed in v2.0.0-dev. Namespace migration tooling is no longer required.'
-);
